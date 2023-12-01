4f0e99d94b53131f51c32305b754d7ad5d3ed737
Nasir Mustafayev <nasir.mustafayev@gmail.com>
2023-12-01 10:26:14 +0400

    Updating styles

--- main.js
+++ main.js
@@ -1,49 +1,54 @@
-let form = document.getElementById("form");
-let title = document.getElementById("taskTitle");
-let date = document.getElementById("taskDate");
-let time = document.getElementById("taskTime");
-let description = document.getElementById("taskDesc");
-let msg = document.getElementById("msg");
-let add = document.getElementById("buttonAdd");
-let taskstext = document.getElementById("taskstext");
+const form = document.getElementById("form");
+const formClass = document.querySelector(".form")
+const title = document.getElementById("taskTitle");
+const date = document.getElementById("taskDate");
+const time = document.getElementById("taskTime");
+const description = document.getElementById("taskDesc");
+const msg = document.getElementById("msg");
+const add = document.getElementById("buttonAdd");
+const taskstext = document.getElementById("taskstext");
+const newTaskBtn = document.querySelector("#newTask")
+
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("Clicked");
-  
-    formValidation();
-  });
-  
-  let formValidation = () => {
-    if(title.value === ""){
-        msg.innerHTML = "Title can't empty";
-        console.log('%c Opssss! ','color:red;font-size: 15px');
-    }
-    else{
-        msg.innerHTML = "";
-        console.log('%c Working fine!', 'color:green;font-size:15px');
-        saveTask();
-    }
-  };
+  e.preventDefault();
+  formValidation();
+});
 
-  let data =[]
+newTaskBtn.addEventListener("click", () => {
+  formClass.classList.toggle("displayNone")
+})
 
-  let saveTask = () =>{
-    data.push({
-        title: title.value,
-        date: date.value,
-        time: time.value,
-        description: description.value
-    });
-
-    localStorage.setItem("taskdata", JSON.stringify(data));
-    console.log(data);
-    showTask();
+let formValidation = () => {
+  if (title.value === "") {
+    msg.innerHTML = "Title can't empty";
+    console.log('%c Opssss! ', 'color:red;font-size: 15px');
   }
+  else {
+    msg.innerHTML = "";
+    console.log('%c Working fine!', 'color:green;font-size:15px');
+    saveTask();
+  }
+};
 
-  let showTask = () =>{
-    tasks.innerHTML = "";
-    data.map((x,y) =>(tasks.innerHTML += `
-      <div id="${y}">
+let data = []
+
+let saveTask = () => {
+  data.push({
+    title: title.value,
+    date: date.value,
+    time: time.value,
+    description: description.value
+  });
+
+  localStorage.setItem("taskdata", JSON.stringify(data));
+  console.log(data);
+  showTask();
+}
+
+let showTask = () => {
+  tasks.innerHTML = "";
+  data.map((x, y) => (tasks.innerHTML += `
+      <div id="${y}" class="task">
       <span id="tasktitle">${x.title}</span>
       <u>${x.date}</u>
       <i>${x.time}</i>
@@ -55,61 +60,50 @@ form.addEventListener("submit", (e) => {
       <button id="delete" onClick ="editTask(this)">&#9998;</button>
       <button id="delete" onClick ="deleteTask(this)">&#10006;</button>
       </div>
-      <hr>
       </div>
       `));
-    add.innerHTML="&#10010;Add";
-    resetForm();
-    checkTaskstext();
-    }
+  add.innerHTML = "&#10010;Add";
+  resetForm();
+}
 
-    
-    let resetForm = () =>{
-      title.value = "";
-      date.value = "";
-      time.value = "";
-      description.value = "";
-    }
 
-    let deleteTask = (e) => {
-      e.parentElement.parentElement.remove();
-     
-      data.splice(e.parentElement.parentElement.id,1);
-  
-      localStorage.setItem("taskdata", JSON.stringify(data));
-  
-      console.log(data);
+let resetForm = () => {
+  title.value = "";
+  date.value = "";
+  time.value = "";
+  description.value = "";
+  formClass.classList.toggle("displayNone")
+}
 
-      checkTaskstext();
-  
-    }
-    let editTask = (e)=>{
-      let task = e.parentElement.parentElement;
+let deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
 
-      title.value = task.children[0].innerHTML;
-      date.value = task.children[1].innerHTML;
-      time.value = task.children[2].innerHTML;
-      //We do different calling method for not see html codes in inside of textarea
-      description.value = task.children[3].children[1].innerHTML;
+  data.splice(e.parentElement.parentElement.id, 1);
 
-      add.innerHTML="&#10004;Save";
-      deleteTask(e);
-      checkTaskstext();
-    }
+  localStorage.setItem("taskdata", JSON.stringify(data));
 
-    let checkTaskstext =()=>{
-      if(data.length===0){
-        taskstext.innerHTML="";
-      }
-      else{
-        taskstext.innerHTML="Tasks";
-      }
-    }
+  console.log(data);
 
-    //Invoke tasks from local stroage
-    (() => {
-      data = JSON.parse(localStorage.getItem("taskdata")) || [];
-      console.log(data);
-      showTask();
-      checkTaskstext();
-    })();
+
+}
+let editTask = (e) => {
+  formClass.classList.toggle("displayNone")
+  let task = e.parentElement.parentElement;
+
+  title.value = task.children[0].innerHTML;
+  date.value = task.children[1].innerHTML;
+  time.value = task.children[2].innerHTML;
+  //We do different calling method for not see html codes in inside of textarea
+  description.value = task.children[3].children[1].innerHTML;
+
+  add.innerHTML = "&#10004;Save";
+  deleteTask(e);
+}
+
+//Invoke tasks from local stroage
+(() => {
+  data = JSON.parse(localStorage.getItem("taskdata")) || [];
+  console.log(data);
+  showTask();
+  formClass.classList.add("displayNone")
+})();
